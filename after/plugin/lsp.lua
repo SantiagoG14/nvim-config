1927606fbf3369c77916ed99d44150f2cf2a5a43
Santiago Garcia <sagaan@outlook.com>
2024-04-20 10:42:05 -0700

    more configs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,30 +3,42 @@ local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {buffer = bufnr})
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {})
-  vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, { buffer = bufnr })
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, {})
+	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
 
 end)
 
 -- set up language server
 mason.setup({})
 mason_lspconfig.setup({
-	ensure_installed = {'tsserver', 'eslint', 'tailwindcss', 'lua_ls'},
+	ensure_installed = { "tsserver", "rust_analyzer", "tailwindcss", "lua_ls" },
 	handlers = {
 		function(server_name)
-			require('lspconfig')[server_name].setup({})
+			require("lspconfig")[server_name].setup({})
 		end,
-	}
+
+		rust_analyzer = function()
+			require("lspconfig").rust_analyzer.setup({
+				on_attach = function(client, bufnr)
+					vim.lsp.inlay_hint.enable()
+				end,
+			})
+		end,
+	},
 })
 
 local cmp = require("cmp")
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
-		['<C-y>'] = cmp.mapping.confirm({select = true}),
-		['<C-Space>'] = cmp.mapping.complete()
-	})
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
 })
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -13,6 +13,13 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 	},
 
+	matchup = {
+		enable = true,
+	},
+
+	incremental_selection = {
+		enable = true,
+	},
 	-- List of parsers to ignore installing (or "all")
 	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
